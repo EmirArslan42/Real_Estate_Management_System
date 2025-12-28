644b56939e25450164558da2545a91fcece5579e
emirarslan <emirarslan179@gmail.com>
2025-12-28 19:11:02 +0300

    Updated calculation module routing

--- TasinmazAngular/src/app/dashboard/hesaplama/hesaplama-routing.module.ts
+++ TasinmazAngular/src/app/dashboard/hesaplama/hesaplama-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AlanHesabiComponent } from './alan-hesabi/alan-hesabi.component';
 
 const routes: Routes = [
-  {path:'', component:AlanHesabiComponent}
+  {path:'', redirectTo:'alan-hesabi',pathMatch:'full'},
+  {path:'alan-hesabi',component:AlanHesabiComponent}
 ];
 
 @NgModule({
