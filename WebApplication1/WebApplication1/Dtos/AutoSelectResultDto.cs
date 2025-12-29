def785318bf87325c1db951179ea055bc080a659
emirarslan <emirarslan179@gmail.com>
2025-12-29 20:07:32 +0300

    Introduce geometry and auto select result DTOs

---
+++ WebApplication1/WebApplication1/Dtos/AutoSelectResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Dtos
+{
+    public class AutoSelectResultDto
+    {
+        public GeometryDto A { get; set; }
+        public GeometryDto B { get; set; }
+        public GeometryDto C { get; set; }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Dtos/GeometryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Dtos
+{
+    public class GeometryDto
+    {
+        public int Id { get; set; }
+        public string Geometry { get; set; }
+    }
+}
