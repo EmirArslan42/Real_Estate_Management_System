0bbfa60f18e145505bb06ae60d0164a3757cdde1
emirarslan <emirarslan179@gmail.com>
2026-01-14 00:03:07 +0300

    chore(db): update entities and add userId to alan analiz sonuclari

---
+++ WebApplication1/WebApplication1/Migrations/20260113103733_UpdateEntities.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageType",
+                table: "Tasinmazlar",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ImageData",
+                table: "Tasinmazlar",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IpAddress",
+                table: "Logs",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Logs",
+                type: "character varying(350)",
+                maxLength: 350,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(350)",
+                oldMaxLength: 350);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageType",
+                table: "Tasinmazlar",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ImageData",
+                table: "Tasinmazlar",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IpAddress",
+                table: "Logs",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Logs",
+                type: "character varying(350)",
+                maxLength: 350,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(350)",
+                oldMaxLength: 350,
+                oldNullable: true);
+        }
+    }
+}
