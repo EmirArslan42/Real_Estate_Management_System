72c10f463fb340d3317be8bc7ed1c834ebefea15
emirarslan <emirarslan179@gmail.com>
2026-01-14 23:51:05 +0300

    chore(dashboard): update dashboard module configuration

--- TasinmazAngular/src/app/dashboard/dashboard.module.ts
+++ TasinmazAngular/src/app/dashboard/dashboard.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardLayoutComponent } from './dashboard-layout/dashboard-layout.component';
 import { ReactiveFormsModule } from '@angular/forms';
